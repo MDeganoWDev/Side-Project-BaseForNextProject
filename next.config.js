8258d5ec2c5c916b341496a1827c81afd3a89cbd
MDegano <123469601+MDeganoWDev@users.noreply.github.com>
2023-06-03 15:55:06 +0200

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: false,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+export default nextConfig;
